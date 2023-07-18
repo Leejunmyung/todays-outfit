4299e3d5e01063c909e79a6b0d49d89f4477bb3f
Leejunmyung <wnsaud9322@naver.com>
2023-07-18 17:40:56 +0900

    파파고 openApi 연동

---
+++ src/components/Translate.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface TranslateProps {
+  text: string | undefined;
+}
+
+const Translate = ({ text }: TranslateProps) => {
+  const [translatedText, setTranslatedText] = useState('');
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      try {
+        const { data } = await axios.post(
+          'https://thingproxy.freeboard.io/fetch/https://openapi.naver.com/v1/papago/n2mt',
+
+          {
+            source: 'en',
+            target: 'ko',
+            text: text,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Naver-Client-Id': process.env.REACT_APP_NAVER_CLIENT_ID,
+              'X-Naver-Client-Secret': process.env.REACT_APP_NAVER_CLIENT_SECRET,
+            },
+          },
+        );
+
+        setTranslatedText(data.message.result.translatedText);
+      } catch (error) {
+        console.error('Failed to translate text.', error);
+      }
+    };
+    if (text) {
+      doTranslation();
+    }
+  }, [text]);
+
+  return <div>{translatedText}</div>;
+};
+
+export default Translate;
