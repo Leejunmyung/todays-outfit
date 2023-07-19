bca3c533604f801c640798ba01e0a0224cf4740a
Leejunmyung <wnsaud9322@naver.com>
2023-07-19 15:34:00 +0900

    번역 컴포넌트 수정

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -12,21 +12,12 @@ const Translate = ({ text }: TranslateProps) => {
     const doTranslation = async () => {
       try {
         const { data } = await axios.post(
-          'https://todays-outfit.vercel.app/serverless/Translate',
+          'https://todays-outfit.vercel.app/serverless/translate',
           // https://openapi.naver.com/v1/papago/n2mt
           // https://thingproxy.freeboard.io/fetch/
 
           {
-            source: 'en',
-            target: 'ko',
             text: text,
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Naver-Client-Id': process.env.REACT_APP_NAVER_CLIENT_ID,
-              'X-Naver-Client-Secret': process.env.REACT_APP_NAVER_CLIENT_SECRET,
-            },
           },
         );
 
