53976c4f10dda30b04262983703d033aaa4ab3eb
Leejunmyung <wnsaud9322@naver.com>
2023-07-30 22:58:23 +0900

    react-router-dom & react-ga 설치 및 코드삽입

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './pages/Home/HomeContainer';
+import { BrowserRouter as Router, Route, useLocation, Routes } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS}`);
+
+function UsePageViews() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <UsePageViews />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
