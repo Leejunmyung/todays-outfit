3b9de91db0a4980fc3c2428d87ae561495bd9c86
Leejunmyung <wnsaud9322@naver.com>
2023-08-04 17:29:35 +0900

    시간 & 요일 추출 함수 추가

--- src/utils/tools/convertDate.ts
+++ src/utils/tools/convertDate.ts
@@ -13,4 +13,17 @@ export const convertDate = {
     };
     return date.toLocaleDateString('ko-KR', options);
   },
+  outputHour(timestamp: number | undefined) {
+    if (timestamp === undefined) return;
+    const date = new Date(timestamp * 1000);
+    return date.getHours();
+  },
+  outputSimpleDate(timestamp: number | undefined) {
+    if (timestamp === undefined) return;
+    const date = new Date(timestamp * 1000);
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    const simpleDate = `${date.getMonth() + 1}.${date.getDate()}`;
+    const day = week[date.getDay()];
+    return { simpleDate, day };
+  },
 };
