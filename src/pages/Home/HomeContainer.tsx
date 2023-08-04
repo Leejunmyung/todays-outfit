5c172e1b16592359c8a4e6a4af08bf297676ee16
Leejunmyung <wnsaud9322@naver.com>
2023-08-04 17:28:41 +0900

    container에 api호출

--- src/pages/Home/HomeContainer.tsx
+++ src/pages/Home/HomeContainer.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import HomePresenter from './HomePresenter';
-import { useGetPollution, useGetWeather } from '../../apiClient/Queries';
-import { weatherData, airPollutionData } from '../../recoil/atom';
+import { useGetPollution, useGetWeather, useGetWeeklyWeather } from '../../apiClient/Queries';
+import { weatherData, airPollutionData, weeklyWeatherData } from '../../recoil/atom';
 import { useRecoilState } from 'recoil';
 import Loading from '../../components/Loading';
 import SplashScreen from '../../components/SplashScreen';
@@ -10,12 +10,14 @@ const DEFAULT_LOCATION = { lat: 37.566535, lon: 126.9779692 };
 
 const HomeContainer = () => {
   const [splashing, setSplashing] = useState(true);
-  const [, setWeathersData] = useRecoilState(weatherData);
+  const [, setWeatherData] = useRecoilState(weatherData);
   const [, setAirPollutionData] = useRecoilState(airPollutionData);
+  const [, setWeeklyWeatherData] = useRecoilState(weeklyWeatherData);
   const [location, setLocation] = useState(DEFAULT_LOCATION);
 
   const weatherQuery = useGetWeather(location);
   const pollutionQuery = useGetPollution(location);
+  const weeklyWeatherQuery = useGetWeeklyWeather(location);
 
   const fetchLocation = () => {
     if (!navigator.geolocation) {
@@ -35,11 +37,12 @@ const HomeContainer = () => {
   };
 
   useEffect(() => {
-    if (weatherQuery.data && pollutionQuery.data) {
-      setWeathersData(weatherQuery.data);
+    if (weatherQuery.data && pollutionQuery.data && weeklyWeatherQuery.data) {
+      setWeatherData(weatherQuery.data);
       setAirPollutionData(pollutionQuery.data);
+      setWeeklyWeatherData(weeklyWeatherQuery.data);
     }
-  }, [weatherQuery.data, pollutionQuery.data]);
+  }, [weatherQuery.data, pollutionQuery.data, weeklyWeatherQuery.data]);
 
   useEffect(() => {
     fetchLocation();
