017de9c75fef3a9ce4fe1007a11eea88f1a6a7d1
Leejunmyung <wnsaud9322@naver.com>
2023-12-19 22:49:36 +0900

    외부 모듈 fetcher axios 3차 수정

--- api/fetcher.js
+++ api/fetcher.js
@@ -6,19 +6,19 @@ module.exports = async (req, res) => {
 
   try {
     const response = await axios.get(
-      `${process.env.REACT_APP_BASE_URL}${url}${text}`,
+      `https://api.openweathermap.org/data/2.5${text}`,
       {
         source: 'en',
         target: 'ko',
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
         params: {
           appid: `${process.env.REACT_APP_API_KEY}`,
           units: 'metric',
           lang: 'kr',
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
         },
       },
     );
