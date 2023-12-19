5e2b30e7b4ef893d21bef33ad57cccb77b629818
Leejunmyung <wnsaud9322@naver.com>
2023-12-19 21:22:09 +0900

    외부 모듈 fetcher axios 수정

--- api/fetcher.js
+++ api/fetcher.js
@@ -1,11 +1,25 @@
 // api/translate.js
-const baseApi = require('../src/apiClient/Fetcher');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
   const { url, params } = req.body;
 
   try {
-    const response = await baseApi.get(url, params);
+    const response = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}${url}`,
+      {
+        source: 'en',
+        target: 'ko',
+        params: params,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+          'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+        },
+      },
+    );
 
     res.status(200).send(response.data);
   } catch (error) {
